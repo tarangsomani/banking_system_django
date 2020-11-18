1f685e9bf9b5b0c51cc7df617cfb3f7fdca1db37
tarangg <somani.tarang@gmail.com>
2020-11-19 01:52:57 +0530

    account - models

--- account/models.py
+++ account/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from utils.models import TimeStampedModel
+from django.contrib.auth.models import User as DjangoUser
 
-# Create your models here.
+
+class User(DjangoUser, TimeStampedModel):
+    MANAGER = 'manager'
+    CUSTOMER = 'customer'
+
+    USER_TYPE = (
+        (MANAGER, 'manager'),
+        (CUSTOMER, 'customer')
+    )
+
+    user_type = models.CharField(choices=USER_TYPE, default=CUSTOMER)
+    mobile = models.CharField(max_length=20, null=True)
+    branch_name = models.CharField(max_length=255, null=True)
+
+
+class Customer(TimeStampedModel):
+    SAVING = 'saving'
+    CURRENT = 'current'
+
+    ACCOUNT_TYPE = (
+        (SAVING, 'saving'),
+        (CURRENT, 'current')
+    )
+
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
+    account_number = models.CharField(max_length=20, unique=True)
+    account_type = models.CharField(choices=ACCOUNT_TYPE, default=SAVING)
+    current_balance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
+
+class Transactions(TimeStampedModel):
+    CREDIT = 'credit'
+    DEBIT = 'debit'
+
+    TRANSACTION_TYPE = (
+        (CREDIT, 'credit'),
+        (DEBIT, 'debit')
+    )
+
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+    transaction_type = models.CharField(max_length=20, choices=TRANSACTION_TYPE)
+    amount = models.DecimalField(max_digits=20, decimal_places=2)
